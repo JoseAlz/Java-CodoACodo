5099aaa5baa4870b8e9b8f4b2ff1cd7b8ce277b4
Marcelo <mlimideiro@gmail.com>
2021-05-13 01:10:35 -0300

    Clase 12

---
+++ Clase 12 - 12-05-21/index.js
@@ -0,0 +1,49 @@
+console.warn("Bienvenidos a codo a codo");
+console.log(2 + 2);
+console.log("Bienvenidos a codo a codo");
+console.error("Bienvenidos a codo a codo");
+console.log("Hola, miren este numero: ", 5 + 48);
+console.info("Hola, miren este numero: ", 5 + 28);
+document.write("Hola, miren este numero: ", 2 + 7); /* ESCRIBE EN LA PANTALLA EL DATO DE CONSOLA */
+
+/* VARIABLES */
+var num = 5
+var letra = "hola"
+
+/*CONSTANTE */
+
+const pi = 3.141592;
+console.log(pi);
+
+console.log(Math.random());
+
+/* OBJETO NUMBER */
+
+var b;
+var b = new Number('123');
+console.log(b);
+
+var a;
+var a = Number('123');
+console.log(b);
+
+/* parseInt */
+console.log(parseInt("f",16));
+console.log(parseInt("15",10));
+console.log(parseInt("11000101001",2));
+
+/* parseFloat */
+
+console.log(parseFloat(Math.PI +16));
+console.log(parseFloat("3.14") + 50);
+
+alert("Prueba de alert");
+
+/* VALOR POR TECLADO  */
+
+nombre = prompt("Ingrese un nombre: ");
+console.log("Su nombre es: ", nombre);
+
+
+
+
