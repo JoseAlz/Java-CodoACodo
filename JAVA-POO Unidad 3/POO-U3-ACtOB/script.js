fcf4ca7a71fbb46778fde3cce132f356eab7a093
Marcelo <mlimideiro@gmail.com>
2021-07-17 20:43:33 -0300

    Terminada Actividad Obligatoria Unidad 3 JAVA-POO

---
+++ JAVA-POO Unidad 3/POO-U3-ACtOB/script.js
@@ -0,0 +1,10 @@
+var titulo = "Marcelo Gastón Limideiro";
+var curso = "JAVA-BACK END 2021";
+var comision = "#Comisión 2107";
+var descripcion = "En este curso se dio contenido sobre HTML, CSS, Bootstrap, JS, GIT, Base de Datos y JAVA-POO."
+
+
+document.write(titulo+"<br><br>");
+document.write("Mi curso es: " + curso + "<br><br>");
+document.write("Me encuentro en la  " + comision + "<br><br>");
+document.write("Descripción del curso: "+ descripcion);
